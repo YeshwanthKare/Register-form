34fc87657c9901b8e680ca0f4fb6eccffd8d5f6f
TommmyKelly <tommy_kelly@icloud.com>
2021-03-08 00:26:27 +0000

    ADD: commit

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-const express = require('express')
-const port = 3001
-const path = require('path')
-const mongoose = require('mongoose');
-const User = require('./model/user')
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const port = 3001;
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./model/user");
+const bcrypt = require("bcryptjs");
 
-const app = express()
+const app = express();
 
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
-app.use('/',express.static(path.join(__dirname,'static')))
+app.use("/", express.static(path.join(__dirname, "static")));
 
 // connect to mongodB
-const dbURI = 'mongodb+srv://testuser:test123@nodetuts.kiou2.mongodb.net/users?retryWrites=true&w=majority'
+const dbURI =
+  "mongodb+srv://testuser:test123@nodetuts.kiou2.mongodb.net/users?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
-  useUnifiedTopology:true,
-  useCreateIndex: true
-})
-
-app.get('/api/reg', (req, res) => {
-  res.status(200).json({success: true})
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.post('/api/reg', (req, res) => {
-  console.log(req.body.username);
+app.get("/api/reg", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
+app.post("/api/reg", (req, res) => {
   res.status(200).json(req.body);
-  console.log(req.body.username);
-})
+  console.log(req.body);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
-
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
